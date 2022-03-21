54c97ba9956ce96f367831e29a17113cf86784af
carlos diaz <diazmelian.carlos@gmail.com>
2022-03-21 17:54:42 +0000

    created basic components

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {
+  Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+  Button, Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label
+} from 'reactstrap';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>The header</div>
+    )
+  }
+}
+
+export default Header
