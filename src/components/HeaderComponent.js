ec4a46634316bcb2aefc59a98100f16312f4a45c
carlos diaz <diazmelian.carlos@gmail.com>
2022-03-22 13:15:56 +0000

    created basic navbar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,61 @@
 import React, { Component } from "react";
 import {
-  Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+  Navbar, NavbarBrand, Nav, NavLink, NavbarToggler, Collapse, NavItem, Jumbotron,
   Button, Modal, ModalHeader, ModalBody,
   Form, FormGroup, Input, Label
 } from 'reactstrap';
 
 class Header extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggleNav = this.toggleNav.bind(this);
+
+    this.state = {
+      isNavOpen: false,
+    };
+
+  }
+
+  toggleNav() {
+    console.log("happens" + this.state.isNavOpen);
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  };
+
   render() {
     return (
-      <div>The header</div>
+      <Navbar color="dark" expand="md">
+        <NavbarBrand>
+          <img src='assets/images/logo.svg' alt='Carlos Diaz portfolio' />
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggleNav} />
+        <Collapse isOpen={this.state.isNavOpen} navbar className="justify-content-end">
+          <Nav
+            navbar
+            fill
+          >
+            <NavItem>
+              <NavLink href="">
+                About
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">
+                Work
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+
+      </Navbar>
     )
   }
 }
