1dbd9062e0bee9d7632a24c04c276501714942ee
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-09 16:25:30 +0100

    added social side bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/HeaderComponent";
 import { FirstSection } from "./components/FirstSectionComponent";
+import { SocialSideBar } from "./components/SocialSideBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <SocialSideBar />
       <Header />
       <FirstSection />
       <FirstSection />
--- src/components/FirstSectionComponent.js
+++ src/components/FirstSectionComponent.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion"
 import {
-  Container, Row, Col, Button
+  Container, Button
 } from 'reactstrap';
 
 
@@ -8,18 +8,18 @@ import {
 
 export const FirstSection = () => {
   return (
-    <Container className="col-12 col-sm-11 col-md-10 col-xl-8 d-flex align-items-center" id="first-section">
+    <Container className="col-10 col-sm-8 col-md-6 d-flex align-items-center" id="first-section">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1, duration: 3, type: "tween" }}
       >
 
-        <h3>Hi, my name is</h3>
+        <h4>Hi, my name is</h4>
         <h1>Carlos Diaz</h1>
-        <h2>I build things for the web</h2>
+        <h3>I build things for the web</h3>
         <p className="text-justify">alotof words all here, chlling out again and again alotof words all here, chlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again</p>
-        <Button>Get In Touch</Button>
+        <Button color="primary">Get In Touch</Button>
       </motion.div>
     </Container>)
 }
