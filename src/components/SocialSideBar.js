1d336b9574938910a9b41dd39d19d106db4131d5
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-10 18:51:55 +0100

    improved social side bar now links work

--- src/components/SocialSideBar.js
+++ src/components/SocialSideBar.js
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const SocialSideBar = () => {
   return (
     <div className='social-bar align-items-center d-none d-md-flex flex-column'>
-      <i className="fa fa-github github-icon pb-2" aria-hidden="true"></i>
-      <i className="fa fa-linkedin linkedin-icon pb-2" aria-hidden="true"></i>
+      <a href="https://github.com/Carlos-Diaz-07" target="_blank" rel="noreferrer">
+        <i href="https://github.com/Carlos-Diaz-07" className="fa fa-github github-icon pb-2" aria-hidden="true"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/carlos-diaz-07/" target="_blank" rel="noreferrer">
+        <i className="fa fa-linkedin linkedin-icon pb-2" aria-hidden="true"></i>
+      </a>
       <div className="line mt-1"></div>
     </div>
   )
