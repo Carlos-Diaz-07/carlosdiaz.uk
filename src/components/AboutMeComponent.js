78e51ef0d111532ec0f7152bffb06a1ad43a3434
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-12 23:41:17 +0100

    styled about me section

--- src/components/AboutMeComponent.js
+++ src/components/AboutMeComponent.js
@@ -13,17 +13,21 @@ export const AboutMeSection = () => {
       <p className="text-justify">alotof words all her again and again alotof words all here, chlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again </p>
       <p>alotof words all here, chlling out again anwords all here, chlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again </p>
       <p>alotof words all here, chlling out again ahlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again </p><p>Some Technologies that I use: Javascript React Gatsby NodeJs</p>
-      <ul class="list-group list-group-horizontal text-center">
-        <li class="list-group-item flex-fill text-color-primary">An item</li>
-        <li class="list-group-item flex-fill text-color-primary">A second item</li>
+      <ul class="list-group list-group-horizontal">
+        <li class="list-group-item flex-fill text-color-primary">JavaScript</li>
+        <li class="list-group-item flex-fill text-color-primary">Ruby</li>
       </ul>
-      <ul class="list-group  list-group-horizontal-sm text-center">
-        <li class="list-group-item flex-fill text-color-primary">An item</li>
-        <li class="list-group-item flex-fill text-color-primary">A second item</li>
+      <ul class="list-group list-group-horizontal-sm">
+        <li class="list-group-item flex-fill text-color-primary">React</li>
+        <li class="list-group-item flex-fill text-color-primary">Ruby on Rails</li>
       </ul>
-      <ul class="list-group list-group-horizontal-md text-center">
-        <li class="list-group-item flex-fill text-color-primary">An item</li>
-        <li class="list-group-item flex-fill text-color-primary">A second item</li>
+      <ul class="list-group list-group-horizontal-md">
+        <li class="list-group-item flex-fill text-color-primary">Next.JS</li>
+        <li class="list-group-item flex-fill text-color-primary">Node.JS</li>
+      </ul>
+      <ul class="list-group list-group-horizontal-md">
+        <li class="list-group-item flex-fill text-color-primary">Next.JS</li>
+        <li class="list-group-item flex-fill text-color-primary">Node.JS</li>
       </ul>
     </Container>)
 }
