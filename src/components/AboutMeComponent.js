f86eddd9860927c6e39eded56bf06b2e11cbcb54
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-17 17:52:26 +0100

    improved style about me section

--- src/components/AboutMeComponent.js
+++ src/components/AboutMeComponent.js
@@ -12,22 +12,24 @@ export const AboutMeSection = () => {
       <hr />
       <p className="text-justify">alotof words all her again and again alotof words all here, chlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again </p>
       <p>alotof words all here, chlling out again anwords all here, chlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again </p>
-      <p>alotof words all here, chlling out again ahlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again </p><p>Some Technologies that I use: Javascript React Gatsby NodeJs</p>
-      <ul class="list-group list-group-horizontal">
-        <li class="list-group-item flex-fill text-color-primary">JavaScript</li>
-        <li class="list-group-item flex-fill text-color-primary">Ruby</li>
+      <p>alotof words all here, chlling out again ahlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again </p>
+      <p>Some Technologies that I use:
+      <ul class="list-group list-group-horizontal-sm text-center align-items-center">
+        <li class="list-group-item w-50 text-color-primary">JavaScript</li>
+        <li class="list-group-item w-50 text-color-primary">Ruby</li>
       </ul>
-      <ul class="list-group list-group-horizontal-sm">
-        <li class="list-group-item flex-fill text-color-primary">React</li>
-        <li class="list-group-item flex-fill text-color-primary">Ruby on Rails</li>
+      <ul class="list-group list-group-horizontal-sm text-center align-items-center">
+        <li class="list-group-item w-50 flex-fill text-color-primary">React</li>
+        <li class="list-group-item w-50 flex-fill text-color-primary">Ruby on Rails</li>
       </ul>
-      <ul class="list-group list-group-horizontal-md">
-        <li class="list-group-item flex-fill text-color-primary">Next.JS</li>
-        <li class="list-group-item flex-fill text-color-primary">Node.JS</li>
+      <ul class="list-group list-group-horizontal-sm text-center align-items-center">
+        <li class="list-group-item w-50 flex-fill text-color-primary">Next.JS</li>
+        <li class="list-group-item w-50 flex-fill text-color-primary">Node.JS</li>
       </ul>
-      <ul class="list-group list-group-horizontal-md">
-        <li class="list-group-item flex-fill text-color-primary">Next.JS</li>
-        <li class="list-group-item flex-fill text-color-primary">Node.JS</li>
+      <ul class="list-group list-group-horizontal-sm text-center align-items-center">
+        <li class="list-group-item w-50 flex-fill text-color-primary">Next.JS</li>
+        <li class="list-group-item w-50 flex-fill text-color-primary">Node.JS</li>
       </ul>
+      </p>
     </Container>)
 }
