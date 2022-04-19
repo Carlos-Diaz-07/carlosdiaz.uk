3ee341db5dafc33252b65992bc96d1809a348aa8
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-19 19:36:22 +0100

    improved side icons style

--- src/components/SocialSideBar.js
+++ src/components/SocialSideBar.js
@@ -4,10 +4,10 @@ export const SocialSideBar = () => {
   return (
     <div className='social-bar align-items-center d-none d-md-flex flex-column'>
       <a href="https://github.com/Carlos-Diaz-07" target="_blank" rel="noreferrer">
-        <i href="https://github.com/Carlos-Diaz-07" className="fa fa-github github-icon pb-2" aria-hidden="true"></i>
+        <i href="https://github.com/Carlos-Diaz-07" className="fa fa-github icon pb-2" aria-hidden="true"></i>
       </a>
       <a href="https://www.linkedin.com/in/carlos-diaz-07/" target="_blank" rel="noreferrer">
-        <i className="fa fa-linkedin linkedin-icon pb-2" aria-hidden="true"></i>
+        <i className="fa fa-linkedin icon pb-2" aria-hidden="true"></i>
       </a>
       <div className="line mt-1"></div>
     </div>
