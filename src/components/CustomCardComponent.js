5f4a686fb57fe47aa1027171f38cefbb613bb050
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-23 13:31:11 +0100

    custom cards now aligned

--- src/components/CustomCardComponent.js
+++ src/components/CustomCardComponent.js
@@ -20,7 +20,7 @@ export const CustomCard = ({ textSide }) => {
       </div>)
   else {
     return (
-      <div className="custom-card mb-5" color="transparent">
+      <div className="custom-card mb-5 align-self-end" color="transparent">
         <img className='card-img ' src="assets/images/spotify-logo.jpg" alt="spotify logo" />
         <div className='card-text-box position-absolute d-flex flex-column top-50 start-0 translate-middle'>
           <p className='card-title text-start w-100'>
--- src/components/WorkComponent.js
+++ src/components/WorkComponent.js
@@ -4,7 +4,7 @@ import { Container } from 'reactstrap';
 
 export const WorkSection = () => {
   return (
-    <Container className="col-10 col-sm-8 col-md-6 d-flex flex-column " id="work">
+    <Container className="col-10 col-sm-8 col-md-6 d-flex flex-column" id="work">
       <h5 className='text-shadow'>Work</h5>
       <hr />
       <CustomCard textSide={"right"}/>
