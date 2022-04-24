2224fc18fb717f58288abef126c83ba83e5e6a03
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-24 19:07:59 +0100

    fixed custom cards uniq key

--- src/components/CustomCardComponent.js
+++ src/components/CustomCardComponent.js
@@ -1,7 +1,7 @@
-export const CustomCard = ({ textSide, project, uniqKey }) => {
+export const CustomCard = ({ textSide, project }) => {
   if (textSide === "right") {
     return (
-      <div className={"custom-card mb-5 align-self-" + (window.innerWidth > 750 ? "start" : "stretch")} color="transparent" key={uniqKey}>
+      <div className={"custom-card mb-5 align-self-" + (window.innerWidth > 750 ? "start" : "stretch")} color="transparent">
         <img className='card-img' src={project.imgUrl} alt="spotify logo" />
         <div className={"card-text-box d-flex flex-column top-50 " + ((window.innerWidth > 750) && "start-100")}>
           <p className='card-title text-end'>
@@ -22,7 +22,7 @@ export const CustomCard = ({ textSide, project, uniqKey }) => {
       </div>)
   } else {
     return (
-      <div className={"custom-card mb-5 align-self-" + (window.innerWidth > 750 ? "end" : "stretch")} color="transparent" key={uniqKey}>
+      <div className={"custom-card mb-5 align-self-" + (window.innerWidth > 750 ? "end" : "stretch")} color="transparent">
         <img className='card-img ' src={project.imgUrl} alt="spotify logo" />
         <div className='card-text-box d-flex flex-column'>
           <p className='card-title text-start w-100'>
--- src/components/WorkComponent.js
+++ src/components/WorkComponent.js
@@ -7,12 +7,12 @@ export const WorkSection = (projects) => {
   const jobs = projects.projects.map((project) => {
     const textSide = project.id % 2 === 0 ? "right" : "left";
     return (
-      <CustomCard textSide={textSide} project={project} uniqKey={uniqid()} />
+      <CustomCard textSide={textSide} project={project} key={uniqid()} />
     );
   });
 
   return (
-    <Container className="col-10 col-sm-8 col-md-6 d-flex flex-column section" id="work-section" key={uniqid()}>
+    <Container className="col-10 col-sm-8 col-md-6 d-flex flex-column section" id="work-section">
       <h5 className='text-shadow'>Work</h5>
       <hr className="mb-3"/>
       {jobs}
