597e44556a0a86965f180272b0029be74f379ed3
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-24 16:14:03 +0100

    fixed card links

--- src/components/CustomCardComponent.js
+++ src/components/CustomCardComponent.js
@@ -2,7 +2,7 @@ export const CustomCard = ({ textSide, project }) => {
   if (textSide === "right")
     return (
       <div className={"custom-card mb-5 align-self-" + (window.innerWidth > 750 ? "start" : "stretch")} color="transparent" key={project.id}>
-        <img className='card-img' src={project.imgUrl}  alt="spotify logo" />
+        <img className='card-img' src={project.imgUrl} alt="spotify logo" />
         <div className={"card-text-box d-flex flex-column top-50 " + ((window.innerWidth > 750) && "start-100")}>
           <p className='card-title text-end'>
             {project.title}
@@ -11,8 +11,12 @@ export const CustomCard = ({ textSide, project }) => {
             {project.description}
           </p>
           <div className='card-icon-wrapper w-100 text-center'>
-            <i href={project.repoUrl} className="fa fa-github card-icon" aria-hidden="true"></i>
-            <i href={project.projectUrl} className="fa fa-external-link card-icon ms-3" aria-hidden="true"></i>
+            <a href={project.repoUrl} target="_blank" rel="noreferrer">
+              <i className="fa fa-github card-icon" aria-hidden="true"></i>
+            </a>
+            <a href={project.projectUrl} target="_blank" rel="noreferrer">
+              <i className="fa fa-external-link card-icon ms-3" aria-hidden="true"></i>
+            </a>
           </div>
         </div>
       </div>)
@@ -28,8 +32,12 @@ export const CustomCard = ({ textSide, project }) => {
             {project.description}
           </p>
           <div className='card-icon-wrapper w-100 text-center'>
-            <i href={project.repoUrl} className="fa fa-github card-icon" aria-hidden="true"></i>
-            <i href={project.projectUrl} className="fa fa-external-link card-icon ms-3" aria-hidden="true"></i>
+            <a href={project.repoUrl} target="_blank" rel="noreferrer">
+              <i className="fa fa-github card-icon" aria-hidden="true"></i>
+            </a>
+            <a href={project.projectUrl} target="_blank" rel="noreferrer">
+              <i className="fa fa-external-link card-icon ms-3" aria-hidden="true"></i>
+            </a>
           </div>
         </div>
       </div>)
--- src/shared/projects.js
+++ src/shared/projects.js
@@ -3,7 +3,9 @@ export const PROJECTS = [
     id: 0,
     title: "SaunaSwap",
     description: "asdasdasdasd asd asd asddsa a asd a add weda adsf af ad cxva seaf ahgr cdva fdg rgar afdg adfgsdhth yhjsfga agh drga g rg gf gs asddsa a asd a add weda adsf af ad cxva seaf ahgr cdva fdg rgar afdg adfgsdhth yhjsfga agh drga\g rg gf gs",
-    imgUrl: "assets/images/spotify-logo.jpg"
+    imgUrl: "assets/images/spotify-logo.jpg",
+    projectUrl: "https://github.com/Carlos-Diaz-07",
+    repoUrl: "https://github.com/Carlos-Diaz-07"
   }, {
     id: 1,
     title: "AssetBase",
