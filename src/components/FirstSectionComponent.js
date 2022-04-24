bc6b42f93b501871209cc229fca7b5309aab5d90
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-24 13:06:08 +0100

    resized button in big screens

--- src/components/FirstSectionComponent.js
+++ src/components/FirstSectionComponent.js
@@ -12,6 +12,6 @@ export const FirstSection = () => {
       <h1 className='text-shadow'>Carlos Diaz</h1>
       <h2 className='text-shadow'>I build things for the web</h2>
       <p className="text-justify">alotof words all here, chlling out again and again alotof words all here, chlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again</p>
-      <Button href="mailto:diazmelian.carlos@gmail.com" className='col-12 col-sm-6 col-md-5 my-btn-primary'>Get In Touch</Button>
+      <Button href="mailto:diazmelian.carlos@gmail.com" className='col-12 col-sm-6 col-md-4 my-btn-primary'>Get In Touch</Button>
     </Container>)
 }
