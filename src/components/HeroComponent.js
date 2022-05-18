d7f9d03aafcff7521816a295e5b05759bb151033
carlos diaz <diazmelian.carlos@gmail.com>
2022-05-18 19:41:44 +0100

    hero section text

--- src/components/HeroComponent.js
+++ src/components/HeroComponent.js
@@ -10,8 +10,8 @@ export const HeroSection = () => {
     <Container className="col-10 col-sm-8 col-md-6 d-flex flex-column align-items-left justify-content-center section first-section" id="first-section">
       <h4 className='text-shadow'>Hi, my name is</h4>
       <h1 className='text-shadow'>Carlos Diaz</h1>
-      <h2 className='text-shadow'>I build things for the web</h2>
-      <p className="text-justify">alotof words all here, chlling out again and again alotof words all here, chlling out again and again alotof words all here, chlling out again and againalotof words all here, chlling out again and again alotof words all here, chlling out again and again</p>
+      <h2 className='text-shadow'>I build things for the Web</h2>
+      <p className="text-justify">I'm a Junior Software Developer currently working for <a className="text-color-primary" href="https://goatagency.com/" target="_blank" rel="noreferrer"> The Goat Agency</a>. I love creating, be it with my hands, my guitar, my 3D printer or coding.</p>
       <Button href="mailto:diazmelian.carlos@gmail.com" className='col-12 col-sm-6 col-md-4 my-btn-primary'>Get In Touch</Button>
     </Container>)
 }
--- src/shared/projects.js
+++ src/shared/projects.js
@@ -4,8 +4,8 @@ export const PROJECTS = [
     title: "SaunaSwap",
     description: "asdasdasdasd asd asd asddsa a asd a add weda adsf af ad cxva seaf ahgr cdva fdg  gs asddsa a asd a add weda adsf af ad cxva seaf ahgr cdva fdg rgar afdg adfgsdhth yhjsfga agh drga rg gf gs",
     imgUrl: "assets/images/spotify-logo.jpg",
-    projectUrl: "https://github.com/Carlos-Diaz-07",
-    repoUrl: "https://github.com/Carlos-Diaz-07"
+    projectUrl: "https://saunaswap.herokuapp.com/saunas",
+    repoUrl: "https://github.com/Johanlynggaard/saunaswap"
   }, {
     id: 1,
     title: "AssetBase",
